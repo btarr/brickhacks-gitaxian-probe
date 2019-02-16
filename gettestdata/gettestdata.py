fce6d88a958b55fd53baf8a4f06eb381afeef01b
Bill Tarr <betarr13@gmail.com>
2019-02-16 16:57:25 -0500

    read in existing json

--- gettestdata/gettestdata.py
+++ gettestdata/gettestdata.py
@@ -4,12 +4,23 @@ import time
 import uuid
 from PIL import Image
 from io import BytesIO
+import os.path
 
 BASE_API_URI = 'https://api.scryfall.com'
 RANDOM_CARD_API = BASE_API_URI + '/cards/random'
-REQUEST_DELAY = 1 #second(s)
+REQUEST_DELAY = .5 #second(s)
 OUTPUT_FOLDER = './output'
 IMAGE_OUTPUT_FOLDER = './output/images'
+NUMBER_OF_CARDS_TO_FETCH = 100
+CARD_OUTPUT_FILE = OUTPUT_FOLDER + '/cards.json'
+
+def log(text, space_below = False):
+  print(text)
+  if space_below:
+    print('##########################################')
+
+def delay_before_request():
+  time.sleep(REQUEST_DELAY)
 
 def save_card_image(card_image, card_id):
   width, height = card_image.size
@@ -27,7 +38,8 @@ def fetch_random_card():
   image_url = card['image_uris']['normal']
 
   # wait to request image so we don't overload the API
-  time.sleep(REQUEST_DELAY)
+
+  delay_before_request()
 
   image_request = requests.get(image_url)
   card_image = Image.open(BytesIO(image_request.content))
@@ -38,9 +50,22 @@ def fetch_random_card():
     "file_name": file_name
   })
 
-if __name__ == "__main__":
+def main():
   cards = []
-  cards.append(fetch_random_card())
+  if os.path.isfile(CARD_OUTPUT_FILE):
+    with open(CARD_OUTPUT_FILE, 'r') as content_file:
+      cards = json.loads(content_file.read())
+  log('Fetching ' + str(NUMBER_OF_CARDS_TO_FETCH) + ' cards', True)
+  for _ in range(0, NUMBER_OF_CARDS_TO_FETCH):
+    log('Fetching card...')
+    delay_before_request()
+    card = fetch_random_card()
+    log('Fetched: ' + card['name'])
+    cards.append(card)
+    log(card['name'] + ' added', True)
   cards_json = json.dumps(cards)
-  with open(OUTPUT_FOLDER + '/cards.json', 'w') as file:
+  with open(CARD_OUTPUT_FILE, 'w') as file:
     file.write(cards_json)
+
+if __name__ == "__main__":
+  main()
