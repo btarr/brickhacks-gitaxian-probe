267911d883107236cf0cfbf550a3ddaeb0cba6f4
Bill Tarr <betarr13@gmail.com>
2019-02-17 09:40:29 -0500

    move all video to own component

--- ui/App.jsx
+++ ui/App.jsx
@@ -1,40 +1,15 @@
 import React, { PureComponent } from 'react'
 import StreamOutput from './components/StreamOutput';
 
-const WEBCAM_CONSTRAINTS = { video: true }
-
 export default class App extends PureComponent {
 
   constructor() {
     super()
-    this.state = {
-      stream: null
-    }
-    this.onWebcamAccess = this.onWebcamAccess.bind(this);
-    this.onNoWebcamAccess = this.onNoWebcamAccess.bind(this);
-  }
-  
-  onWebcamAccess(stream) {
-    if (stream) {
-      this.setState({
-        stream: stream
-      });
-    }
-  }
-  
-  onNoWebcamAccess() {
-
-  }
-
-  componentDidMount() {
-    navigator.getUserMedia(WEBCAM_CONSTRAINTS, this.onWebcamAccess, this.onNoWebcamAccess);
   }
 
   render() {
     return (
-      <StreamOutput 
-        src={this.state.stream}
-      />
+      <StreamOutput />
     )
   }
 }
--- ui/components/StreamOutput.jsx
+++ ui/components/StreamOutput.jsx
@@ -1,31 +1,81 @@
 import React, { PureComponent } from 'react';
+import { List } from 'immutable';
+
+const WEBCAM_CONSTRAINTS = { video: true }
 
 export default class StreamOutput extends PureComponent {
 
   constructor() {
     super()
     this.videoRef = React.createRef();
+    this.state = {
+      videoOn: false,
+      images: List(),
+      stream: null,
+      noWebcamAccess: false,
+    }
+    this.track = null;
+    this.captureWebcamImage = this.captureWebcamImage.bind(this);
+    this.updateVideoStream = this.updateVideoStream.bind(this);
+    this.onWebcamAccess = this.onWebcamAccess.bind(this);
+    this.onNoWebcamAccess = this.onNoWebcamAccess.bind(this);
+  }
+
+  onWebcamAccess(stream) {
+    if (stream) {
+      this.setState({
+        stream: stream
+      });
+    }
+  }
+
+  onNoWebcamAccess() {
+    this.setState({
+      noWebcamAccess: true,
+    })
+  }
+
+  componentDidMount() {
+    navigator.getUserMedia(WEBCAM_CONSTRAINTS, this.onWebcamAccess, this.onNoWebcamAccess);
   }
 
   componentDidUpdate() {
-    if (this.videoRef) {
+    if (this.videoRef && this.videoRef.current) {
       this.updateVideoStream();
+      if (!this.state.videoOn) {
+        this.setState({
+          videoOn: true,
+        })
+      }
     }
+  }
+
+  captureWebcamImage(callback) {
+    this.imageCapture.takePhoto().then(callback);
   }
 
   updateVideoStream() {
-    if (this.videoRef.current.srcObject !== this.props.src) {
-      this.videoRef.current.srcObject = this.props.src
+    if (this.videoRef.current.srcObject !== this.state.stream) {
+      this.videoRef.current.srcObject = this.state.stream
+      this.track = this.state.stream.getVideoTracks()[0];
+      this.imageCapture = new ImageCapture(this.track);
     }
   }
 
+  renderNoAccess() {
+    return null;
+  }
+
   render() {
-    const { src } = this.props;
-    if (!src) {
+    const { stream } = this.state;
+    if (!stream) {
       return null;
     }
+    if (this.state.noWebcamAccess) {
+      return this.renderNoAccess();
+    }
     return (
-      <video ref={this.videoRef} src={this.props.src} autoPlay={true} />
+      <video ref={this.videoRef} src={stream} autoPlay={true} />
     );
   }
 }
